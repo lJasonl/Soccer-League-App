ddd59fb56604a884995f28f28e1bb5b7d9a20c25
lJasonl <jgibbs33@liberty.edu>
2026-06-12 10:54:02 -0400

    Added Teams module and Team Details

---
+++ mobile/lib/data/sample_players.dart
@@ -0,0 +1,46 @@
+import '../models/player.dart';
+
+const List<Player> samplePlayers = [
+  Player(
+    id: '1',
+    teamId: '1',
+    name: 'Liam',
+    jerseyNumber: 7,
+  ),
+  Player(
+    id: '2',
+    teamId: '1',
+    name: 'Noah',
+    jerseyNumber: 10,
+  ),
+  Player(
+    id: '3',
+    teamId: '1',
+    name: 'Mason',
+    jerseyNumber: 14,
+  ),
+  Player(
+    id: '4',
+    teamId: '2',
+    name: 'Jackson',
+    jerseyNumber: 4,
+  ),
+  Player(
+    id: '5',
+    teamId: '2',
+    name: 'Ethan',
+    jerseyNumber: 9,
+  ),
+  Player(
+    id: '6',
+    teamId: '3',
+    name: 'Logan',
+    jerseyNumber: 11,
+  ),
+  Player(
+    id: '7',
+    teamId: '4',
+    name: 'Lucas',
+    jerseyNumber: 8,
+  ),
+];
---
+++ mobile/lib/models/player.dart
@@ -0,0 +1,13 @@
+class Player {
+  final String id;
+  final String teamId;
+  final String name;
+  final int jerseyNumber;
+
+  const Player({
+    required this.id,
+    required this.teamId,
+    required this.name,
+    required this.jerseyNumber,
+  });
+}
